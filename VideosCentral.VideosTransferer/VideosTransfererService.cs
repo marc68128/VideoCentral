38ec81f17031715c4aec22d8b5eb5ddac368a9d1
Marc Unterseh <marc.unterseh@i-datech.fr>
2018-03-14 14:13:46 +0100

    Init VideoTransferer

--- VideosCentral.VideosTransferer/VideosTransfererService.cs
+++ VideosCentral.VideosTransferer/VideosTransfererService.cs
@@ -1,48 +1,100 @@
 ﻿using System;
+using System.Configuration;
 using System.IO;
+using System.Linq;
 using System.Management;
 using System.ServiceProcess;
 using System.Timers;
 using VideosCentral.Domain.Enum;
+using VideosCentral.Domain.Model;
 using VideosCentral.Kernel;
 using VideosCentral.Logger;
 using VideosCentral.Logger.Interface;
+using VideosCentral.Services;
+using VideosCentral.Services.Contracts;
 
 namespace VideosCentral.VideosTransferer
 {
     public partial class VideosTransfererService : ServiceBase
     {
-        private ManagementEventWatcher watcher;
+        private readonly IConfigurationFileService _configurationFileService;
+        private readonly IUsbDevicesService _usbDeviceService;
+        private readonly IVideoFileService _videoFileService;
+        private readonly ILogger _logger;
+
         public VideosTransfererService()
         {
-            KernelConfig.RegisterInstance<ILogger>(new FileLogger(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Logs")));
+            var encryptionService = new EncryptionService();
+
+            _videoFileService = new VideoFileService();
+            _configurationFileService = new ConfigurationFileService(encryptionService, _videoFileService);
+            _usbDeviceService = new UsbDevicesService();
+            _logger = new FileLogger(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Logs"));
+
+            KernelConfig.RegisterInstance<ILogger>(_logger);
+            KernelConfig.RegisterInstance<IEncryptionService>(encryptionService);
+            KernelConfig.RegisterInstance<IVideoFileService>(_videoFileService);
+            KernelConfig.RegisterInstance<IConfigurationFileService>(_configurationFileService);
+            KernelConfig.RegisterInstance<IUsbDevicesService>(_usbDeviceService);
+
             InitializeComponent();
         }
 
         protected override void OnStart(string[] args)
         {
-            watcher = new ManagementEventWatcher();
-            WqlEventQuery query = new WqlEventQuery("SELECT * FROM Win32_VolumeChangeEvent");
-            watcher.EventArrived += new EventArrivedEventHandler(DriveChanged);
-            watcher.Query = query;
-            watcher.Start();
-            Kernel.Kernel.Resolve<ILogger>().LogInfo("Service started");
+            _usbDeviceService.DriveInsertedEvent += DriveInserted;
+            _logger.LogInfo("Service started");
         }
 
         protected override void OnStop()
         {
-            watcher.Stop();
-            Kernel.Kernel.Resolve<ILogger>().LogInfo("Service stopped");
+            _usbDeviceService.DriveInsertedEvent -= DriveInserted;
+            _logger.LogInfo("Service stopped");
         }
 
-        private void DriveChanged(object sender, EventArrivedEventArgs e)
+        private void DriveInserted(object sender, string s)
         {
-            string driveName = e.NewEvent.Properties["DriveName"].Value.ToString();
-            DriveEventType eventType = (DriveEventType)(Convert.ToInt16(e.NewEvent.Properties["EventType"].Value));
+            _logger.LogInfo($"\"{s}\" : Inserted");
 
-            string eventName = Enum.GetName(typeof(DriveEventType), eventType);
+            var configurationFile = _configurationFileService.GetConfigurationFile(s);
+            if (configurationFile == null)
+            {
+                _logger.LogInfo($"\"{s}\" : No configuration file found on drive - Use the CameraConfigurator to create one.");
+                return;
+            }
 
-            Kernel.Kernel.Resolve<ILogger>().LogInfo($"\"{driveName}\" : {eventName}");
+            var videosOnDrive = _videoFileService.GetAllVideoFiles(s);
+            var videosInConfig = configurationFile.VideoPaths.ToList();
+
+            var newVideos = videosOnDrive.Except(videosInConfig).ToList();
+
+            if (!newVideos.Any())
+            {
+                _logger.LogInfo($"\"{s}\" : There's no new videos on the drive - Nothing to copy.");
+                return;
+            }
+
+            foreach (var newVideo in newVideos)
+            {
+                var ext = Path.GetExtension(newVideo); 
+                var destinationFolder = ConfigurationManager.AppSettings["VideoFolderPath"];
+                var destinationName = $"{DateTime.Now:yyy-MM-dd-HH-mm-ss}_{configurationFile.LastName}_{configurationFile.FirstName}{ext}";
+                var destinationPath = Path.Combine(destinationFolder, destinationName); 
+
+                if (!Directory.Exists(destinationFolder))
+                    Directory.CreateDirectory(destinationFolder);
+
+                _logger.LogInfo($"\"{s}\" : Start copying {newVideo} to {destinationPath} [{DateTime.Now:T}].");
+                File.Copy(newVideo, destinationPath);
+                _logger.LogInfo($"\"{s}\" : End copying {newVideo} to {destinationPath} [{DateTime.Now:T}].");
+
+                videosInConfig.Add(newVideo);
+
+                _configurationFileService.CreateConfigurationFile(s, new Domain.Model.Configuration(configurationFile.LastName, configurationFile.FirstName, configurationFile.LicenceNumber, videosInConfig));
+
+                _logger.LogInfo($"\"{s}\" : Configuration file updated.");
+            }
         }
+
     }
 }
